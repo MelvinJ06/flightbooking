425722056257e429d5a14489704ac03af407b95f
MelvinJ06 <melvinjames0602@gmail.com>
2024-12-14 12:26:14 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,33 +2,27 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-// Register User
 exports.registerUser = async (req, res) => {
     const { name, email, password } = req.body;
     try {
-        // Check if the user already exists
         const userExists = await User.findOne({ email });
         if (userExists) {
             return res.status(400).json({ message: "The entered user already exists" });
         }
 
-        // Hash the password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        // Create the new user
         const user = await User.create({
             name,
             email,
             password: hashedPassword,
         });
 
-        // Generate a JWT token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
             expiresIn: "1h",
         });
 
-        // Respond with the token and user details
         res.status(201).json({
             token,
             user: {
@@ -42,28 +36,24 @@ exports.registerUser = async (req, res) => {
     }
 };
 
-// Login User
 exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
     try {
-        // Find the user by email
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ message: "Invalid credentials" });
         }
 
-        // Compare the entered password with the stored hashed password
+
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ message: "Invalid password" });
         }
 
-        // Generate a JWT token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
             expiresIn: "1h",
         });
 
-        // Respond with the token and user details
         res.status(200).json({
             token,
             user: {
