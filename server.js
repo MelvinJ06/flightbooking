104952c9738de6a6ce35bf49de770d5f8f7b584f
MelvinJ06 <melvinjames0602@gmail.com>
2024-12-14 17:00:15 +0530

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express(); 
 
 const corsOptions = {
-  origin: "https://flightbooking-2.onrender.com", 
+  origin: ["https://flightbooking-2.onrender.com","https://flight67.netlify.app" ],
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type, Authorization",
   credentials: true, 
